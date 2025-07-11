396f392f7f2e9da87a8fb507b1d5d9884ad511e8
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-07-11 19:47:19 +0200

    start develop new class
    start develop new class

---
+++ classes/reverse_main.py
@@ -0,0 +1,19 @@
+from classes.reverse_move_finder import ReverseMoveFinder
+
+myworkpath = "C:\\Users\\Evert Jan\\pythonprojects\\chesspython_nogithub"
+mypositionfilename = "test_reverse_move_finder"
+n_plies = 6
+
+rmf = ReverseMoveFinder(myworkpath)
+rmf.MyChessGame.LoadFromJsonFile(".\\games\\unittestgame.json", f"{myworkpath}\\positions\\{mypositionfilename}.json")
+rmf.MyChessGame.SaveAsJsonFile(f"{myworkpath}\\games_verify\\unittestgame.json", f"{myworkpath}\\positions_verify\\{mypositionfilename}.json")
+
+myval, mymvidx, _ = rmf.MyChessGame.Calculation_n_plies(n_plies)
+s = f"List : {rmf.MyChessGame.mainposition.DisplayMovelist(rmf.MyChessGame.piecetypes)}"
+print(s)
+
+try:
+    mymvstr = rmf.MyChessGame.mainposition.movelist[mymvidx].ShortNotation(rmf.MyChessGame.piecetypes)
+except:
+    mymvstr = "No move"
+print(f"Result of evaluation {n_plies} plies test_reverse_move_finder: {myval} {mymvstr}")
---
+++ classes/reverse_move_finder.py
@@ -0,0 +1,5 @@
+from classes.chess_game import ChessGame
+
+class ReverseMoveFinder:
+    def __init__(self, pworkpath):
+        self.MyChessGame = ChessGame(pworkpath)
