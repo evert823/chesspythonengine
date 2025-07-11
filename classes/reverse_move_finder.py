04df279d90739c8f5a04c461c14a88f9e891dea0
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2025-07-11 21:10:38 +0200

    one empty new method

--- classes/reverse_move_finder.py
+++ classes/reverse_move_finder.py
@@ -3,3 +3,11 @@ from classes.chess_game import ChessGame
 class ReverseMoveFinder:
     def __init__(self, pworkpath):
         self.MyChessGame = ChessGame(pworkpath)
+
+    def GPPN(self):
+        """
+        Or GeneratePossiblePreviousNodes:
+        Generate all potential previous positions that are legal,
+        and that transition to current position by one legal move
+        """
+        pass
