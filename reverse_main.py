d5b523bbeea7d68c004ff233c9275c5c9b2ff522
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2026-07-20 11:09:33 +0200

    correct wrong update

--- reverse_main.py
+++ reverse_main.py
@@ -5,13 +5,14 @@ def Test_GPPN(mypositionfilename):
     print(f"mypositionfilename {mypositionfilename}")
 
     rmf = ReverseMoveFinder(myworkpath, myjsonsourcepath)
-    rmf.MyChessGame.LoadFromJsonFile(f"{myjsonsourcepath}\\games\\unittestgame.json",
-                                     f"{myworkpath}\\positions\\{mypositionfilename}.json")
-    rmf.MyChessGame.SaveAsJsonFile(f"{myworkpath}\\games_verify\\unittestgame.json",
-                                   f"{myworkpath}\\positions_verify\\{mypositionfilename}.json")
+    rmf.MyChessGame.LoadFromJsonFile(os.path.join(myjsonsourcepath, "games", "unittestgame.json"),
+                                     os.path.join(myworkpath, "positions", f"{mypositionfilename}.json"))
+    rmf.MyChessGame.SaveAsJsonFile(os.path.join(myworkpath, "games_verify", "unittestgame.json"),
+                                   os.path.join(myworkpath, "positions_verify", f"{mypositionfilename}.json"))
 
     rmf.GPPN(rmf.MyChessGame.mainposition)
 
+#myworkpath = os.path.join(os.sep, "home", "administrator", "pythonwork")
 myworkpath = os.path.join("C:\\", "Users", "Evert Jan", "pythonprojects", "chesspython_nogithub")
 myjsonsourcepath = os.path.join("C:\\", "Users", "Evert Jan", "Documents", "GitHub", "chesspython")
 Test_GPPN("fide_fun")
